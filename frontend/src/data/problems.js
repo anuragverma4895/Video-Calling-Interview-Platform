b5bfd2231afdfdc55c4db06cae2c6ee475f29895
Anurag Verma <your-copied-email@users.noreply.github.com>
2026-08-29 23:40:14 +0530

    chore: update problems data

--- frontend/src/data/problems.js
+++ frontend/src/data/problems.js
@@ -5,14 +5,76 @@ import { MEDIUM_PROBLEMS_1 } from "./mediumProblems1.js";
 import { MEDIUM_PROBLEMS_2 } from "./mediumProblems2.js";
 import { HARD_PROBLEMS } from "./hardProblems.js";
 
-export const PROBLEMS = {
-  ...EASY_PROBLEMS_1,
-  ...EASY_PROBLEMS_2,
-  ...EASY_PROBLEMS_3,
-  ...MEDIUM_PROBLEMS_1,
-  ...MEDIUM_PROBLEMS_2,
-  ...HARD_PROBLEMS,
-};
+const EASY_PROBLEMS = [
+  ...Object.values(EASY_PROBLEMS_1),
+  ...Object.values(EASY_PROBLEMS_2),
+  ...Object.values(EASY_PROBLEMS_3),
+];
+
+const MEDIUM_PROBLEMS = [
+  ...Object.values(MEDIUM_PROBLEMS_1),
+  ...Object.values(MEDIUM_PROBLEMS_2),
+];
+
+const HARD_PROBLEMS_LIST = Object.values(HARD_PROBLEMS);
+
+function sortByOriginalOrder(problems) {
+  return [...problems].sort((a, b) => (a.order || 0) - (b.order || 0));
+}
+
+function buildMixedProblemList() {
+  const groups = {
+    Easy: sortByOriginalOrder(EASY_PROBLEMS),
+    Medium: sortByOriginalOrder(MEDIUM_PROBLEMS),
+    Hard: sortByOriginalOrder(HARD_PROBLEMS_LIST),
+  };
+
+  const difficultyPattern = [
+    "Hard",
+    "Medium",
+    "Easy",
+    "Medium",
+    "Hard",
+    "Easy",
+    "Medium",
+    "Easy",
+    "Hard",
+    "Medium",
+    "Easy",
+    "Medium",
+    "Hard",
+    "Easy",
+    "Medium",
+    "Easy",
+    "Hard",
+    "Medium",
+    "Easy",
+    "Medium",
+  ];
+  const totalProblems = Object.values(groups).reduce((total, group) => total + group.length, 0);
+  const mixedProblems = [];
+  let patternIndex = 0;
+
+  while (mixedProblems.length < totalProblems) {
+    const difficulty = difficultyPattern[patternIndex % difficultyPattern.length];
+    const group = groups[difficulty];
+
+    if (group?.length) {
+      mixedProblems.push(group.shift());
+    }
+
+    patternIndex += 1;
+  }
+
+  return mixedProblems.map((problem, index) => ({
+    ...problem,
+    order: index + 1,
+  }));
+}
+
+export const PROBLEMS = Object.fromEntries(
+  buildMixedProblemList().map((problem) => [problem.id, problem])
+);
 
 export const LANGUAGE_CONFIG = {
   javascript: {
@@ -56,3 +118,4 @@ export const LANGUAGE_CONFIG = {
     fileExt: ".cpp",
   },
 };
+
